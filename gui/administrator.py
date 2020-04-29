c712211c7f05d20fa694078574bea095e437028c
Bojan Vasic <bojan.vasic01@gmail.com>
2020-04-29 14:16:55 +0200

    Dodata pocetna forma, i log in deo

---
+++ gui/administrator.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+
+
+
+def poziv_forme_administrator():
+    root = Tk()
+    root.geometry('400x190')
+    root.mainloop()
+
+if __name__ == '__main__':
+    poziv_forme_administrator()
+
--- gui/login.py
+++ gui/login.py
@@ -1,44 +1,80 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+from gui.lekar import poziv_forme_lekar
+from gui.sektretar import poziv_forme_sekretar
+from gui.pacijent import poziv_forme_pacijent
+from gui.administrator import poziv_forme_administrator
+from gui.upravnik import poziv_forme_upravnik
+from gui.neregistrovan import poziv_forme_neregistrovan
+from model.kreiranje_objekata_entiteta import lista_ucitanih_korisnika as lista_korisnika
+
 
 class LogIn:
-    kor_ime = "admin"
-    loz = "admin"
 
     def __init__(self, root):
         self._root = root
-        self._root.title("LOGIN SCREEN")
+        self._root.title("PRIJAVA KORISNIKA")
 
-        Label(text="Korisnicko ime:", font="Times 15").grid(row=1, column=1, pady=20)
-        self.korisnicko_ime = Entry()
-        self.korisnicko_ime.grid(row=1, column=2, columnspan=10)
+        self.__postava_oko_korisnickog_imena()
+        self.__postava_oko_lozinke()
+        self.__postavka_check_dugmeta()
+        self.__postava_oko_dugmeta()
 
-        Label(text="Lozinka:", font="Times 15").grid(row=2, column=1, pady=20)
-        self.lozinka = Entry(show="*")
-        self.lozinka.grid(row=2, column=2, columnspan=10)
+    def __postava_oko_korisnickog_imena(self):
+        Label(self._root, text="Korisnicko ime:", font="Times 15").grid(row=1, column=1, pady=10)
+        self._korisnicko_ime = ttk.Entry()
+        self._korisnicko_ime.grid(row=1, column=2, columnspan=10)
 
-        ttk.Button(text="LOGIN", command=self.log_korisnik).grid(row=3, column=2)
+    def __postava_oko_lozinke(self):
+        Label(self._root, text="Lozinka:", font="Times 15").grid(row=2, column=1, pady=10)
+        self._lozinka = ttk.Entry(show="*")
+        self._lozinka.grid(row=2, column=2, columnspan=10)
 
-    def log_korisnik(self):
+    def __postavka_check_dugmeta(self):
+        self._obelezeno = BooleanVar()
+        self._neregistrovan = ttk.Checkbutton(self._root, text="zelite biti neregistrovani", variable=self._obelezeno,
+                                              offvalue=False, onvalue=True)
+        self._neregistrovan.grid(row=3, column=1)
 
-        self.message = Label(text="               ")
-        self.message.grid(row=6, column=2)
+    def __postava_oko_dugmeta(self):
+        ttk.Button(self._root, text="PRIJAVA", command=self.__prijava).grid(row=3, column=2)
 
-        if self.korisnicko_ime.get() == self.kor_ime and self.lozinka.get() == self.loz:
-
-            self.message = Label(text="Uspeo")
-            self.message.grid(row=6, column=2)
-
-
+    def __prijava(self):
+        if self._obelezeno.get() == True:
+            poziv_forme_neregistrovan()
         else:
+            i = self.__provera_unosa()
+            if (i != None):
+                self.__poziv_forme_za(i)
+            else:
+                messagebox.showerror("GRESKA", "Neispravan unos.")
 
-            self.message = Label(text="Pogresno")
-            self.message.grid(row=6, column=2)
+    def __provera_unosa(self):
+        for i in lista_korisnika:
+            if (i.get_korisnicko_ime() == self._korisnicko_ime.get() and i.get_lozinka() == self._lozinka.get()):
+                return i.get_uloga()
+        return None
+
+    def __poziv_forme_za(self, uloga):
+        if (uloga == "upravnik"):
+            poziv_forme_upravnik()
+        elif (uloga == "sekretar"):
+            poziv_forme_sekretar()
+        elif (uloga == "pacijent"):
+            poziv_forme_pacijent()
+        elif (uloga == "lekar"):
+            poziv_forme_lekar()
+        elif (uloga == "administrator"):
+            poziv_forme_administrator()
 
 
-if __name__ == '__main__':
+def kreni_login():
     root = Tk()
-    root.geometry('425x425')
+    root.geometry('370x130')
     application = LogIn(root)
     root.mainloop()
 
+
+if __name__ == '__main__':
+    kreni_login()
